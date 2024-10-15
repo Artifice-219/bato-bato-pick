4197879778c84d36b3a2544f6383f059cf9822a3
Artifice-219 <johnphilliplormalbas@gmail.com>
2024-10-15 22:24:15 +0800

    added a user and computer class

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-# *TODO: 0 ( 2/2 )
-# TODO 7 : TRACK THE NUMBER OF WINS, might also need a computer class for this  WHOEVER GOT 3 CORRECT WINS WOULD BE WINNER AND THE GAME ENDS
-# TODO 9 : REMEMBER HAVE A MAIN FUNCTION THAT WILL SERVE AS THE CODE DRIVER
+# *TODO: 0 ( 3/3 )
+# TODO ENDER: MOVED ON TO THE NEXT PROJECT AS ANYTHING AFTER THIS IS FOR THE NEXT DESIGN ITERATION
 
 
 choices = ['rock', 'paper', 'scissors']
 
-
+# TODO 14: HAVE SOME DEFAULT VALUES HERE PARA DI MO NA NEED MAG PASS NG 0 VALUES WHEN CREATING A NEW OBJECT
+# TODO 15: ITS NOT A RIGHT-MINUS-WRONG SETUP NEED PA BA TALAGA YUNG MINUS_SCORE()
 class Player:
-    def __init__(self, life):
+    def __init__(self, life, score):
         self.life = 3
+        self.score = 0
     # methods
     def get_life(self):
         return self.life
     
     def decrement_life(self):
         self.life -= 1
+        
+    def add_score(self):
+        self.score += 1
     
+    def minus_score(self):
+        self.score += 1
     
+    def get_score(self):
+        return self.score
+
+class Computer:
+    def __init__(self, score):
+        self.score = 0
+    # methods
+    def add_score(self):
+        self.score += 1
+    
+    def minus_score(self):
+        self.score += 1
+    
+    def get_score(self):
+        return self.score
+
     
 def random_num():
     import random
@@ -37,33 +59,53 @@ def user_choice():
             # check if user choice is allowed
             if(choice in choices):
                 return choice
-                #TODO 8 : CHECK IF THE BREAK STATEMENT HERE IS UNREACHABLE
                 #TODO 9 : HAVE A USER PROMPT NA MALING INPUT SYA
-                break
 
+# TODO 13: YOU INITIALIZED A USER AND PLAYER CLASS HERE, RETHINK IF MAY MAS BETTER NA APPROACH
+new_player = Player(0,0)
+new_computer = Computer(0)
 
 def who_wins(user, computer):
+
+    
     
     # TODO 10 : FIND A WAY TO BETTER CODE THIS SHIT
+    # TODO 16: HAVE A MUCH BETTER WAY TO OUTPUT THAN THIS
     print(f'Youre choice = {user} , Computer choice = {computer} \n')
     
     if(user == computer):
         print("Its a tie")
     elif(user == 'rock' and computer == 'scissors'):
         print('You win')
+        new_player.add_score()
+        
     elif(user == 'paper' and computer == 'rock'):
         print('You win')
+        new_player.add_score()
+        
     elif(user == 'paper' and computer == 'scissors'):
         print('You lose')
+        new_computer.add_score()
+        
     elif(user == 'scissors' and computer == 'paper'):
         print('You win')
+        new_player.add_score()
+        
     else:
         # *possible dito ka na mag decrement
         # TODO 12: DECREMENT USER LIFE EACH TIME THIS CODE RUNS IT MEANS TALO KASI
         print('You lose')
+        new_computer.add_score()
+                
+    print(f'Current score player : {new_player.get_score()} computer : {new_computer.get_score()}')
 
-user = user_choice()
-computer = com_choice()
+def main():
+    
+    while((new_player.get_score() != 3 ) and (new_computer.get_score() != 3) ):
+        user = user_choice()
+        computer = com_choice()
 
-who_wins(user, computer)
+        who_wins(user, computer)
 
+
+main()
